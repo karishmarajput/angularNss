badfe6f644bf88a856e7228ae294c8360956a945
karishmarajput <karrajput@gmail.com>
2023-06-13 11:54:52 +0530

    volunteer table

--- src/app/admin/display-volunteer/display-volunteer.component.ts
+++ src/app/admin/display-volunteer/display-volunteer.component.ts
@@ -1,29 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+interface Volunteer {
+  id: string;
+  vec: string;
+  name: string;
+  DOB: string;
+}
+
 @Component({
   selector: 'app-display-volunteer',
   templateUrl: './display-volunteer.component.html',
   styleUrls: ['./display-volunteer.component.css']
 })
-export class DisplayVolunteerComponent {
-  volunteer:any
-  ngOnInit() {
-    
+export class DisplayVolunteerComponent implements OnInit {
+  volunteer: Volunteer[] = [];
+  filteredVolunteers: Volunteer[] = [];
+  pageSize = 10;
+  currentPage = 0;
+  searchText = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
     this.getVolunteers();
   }
-  constructor(private http: HttpClient,private router: Router) {}
-  goToAddVolunteer() {
+
+  goToAddVolunteer(): void {
     this.router.navigate(['/adminDashboard/addVolunteer']);
   }
-  getVolunteers(){
+
+  getVolunteers(): void {
     this.http.get('http://localhost:3000/api/volunteers').subscribe(
-      (response:any)=>{
+      (response: any) => {
         this.volunteer = response.data.volunteers;
+        this.filteredVolunteers = this.volunteer.slice();
+        this.currentPage = 0;
       },
-      (error:any)=>{
-        console.log('Error: ', error)
+      (error: any) => {
+        console.log('Error: ', error);
       }
-    )
+    );
+  }
+
+  getPaginatedVolunteers(): Volunteer[] {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.filteredVolunteers.slice(startIndex, endIndex);
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.filteredVolunteers.length / this.pageSize);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.getTotalPages() - 1) {
+      this.currentPage++;
+    }
+  }
+
+  filterVolunteers(): void {
+    const searchText = this.searchText.toLowerCase().trim();
+    if (searchText === '') {
+      this.filteredVolunteers = this.volunteer.slice();
+    } else {
+      this.filteredVolunteers = this.volunteer.filter(
+        volunteer =>
+          volunteer.vec.toLowerCase().includes(searchText) ||
+          volunteer.name.toLowerCase().includes(searchText)
+      );
+    }
+    this.currentPage = 0;
+  }
+
+  clearSearch(): void {
+    this.searchText = '';
+    this.filteredVolunteers = this.volunteer.slice();
+    this.currentPage = 0;
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,8 +8,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 @NgModule({
-  imports: [MatSlideToggleModule,MatCardModule,MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule,MatTableModule],
-  exports: [MatSlideToggleModule,MatCardModule,MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule,MatTableModule]
+  imports: [MatSlideToggleModule,MatCardModule,MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule,MatTableModule,MatPaginatorModule],
+  exports: [MatSlideToggleModule,MatCardModule,MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule,MatTableModule,MatPaginatorModule]
 })
 export class MaterialModule {}
